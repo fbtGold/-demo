a455603aac8d750495bcd033316cede8ed5d6564
fbt <fbtgold@foxmail.com>
2020-11-25 08:42:01 +0800

    添加项目文件。

---
+++ 单元测试demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 单元测试demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
