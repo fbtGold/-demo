e4a5d15bbe865543a55aa841309c5d03d0279574
fbt <fbtgold@foxmail.com>
2020-11-25 09:01:52 +0800

    第一次上传

--- 单元测试demo/Program.cs
+++ 单元测试demo/Program.cs
@@ -10,6 +10,47 @@ namespace 单元测试demo
     {
         static void Main(string[] args)
         {
+            var powerSupply = new PowerSupply();
+            DeskFan deskFan = new DeskFan(powerSupply);
+            Console.WriteLine(deskFan.word());
+        }
+    }
+    class PowerSupply
+    {
+        public int GoPower()
+        {
+            return 100;
+        }
+    }
+
+    class DeskFan
+    {
+        public PowerSupply _powerSupply;
+
+        public DeskFan(PowerSupply powerSupply)
+        {
+            _powerSupply = powerSupply;
+        }
+
+        public string word()
+        {
+            int power = _powerSupply.GoPower();
+            if (power<=0)
+            {
+                return "没电";
+            }
+            else if (power <=100)
+            {
+                return "慢转";
+            }
+            else if(power <=200)
+            {
+                return "快转";
+            }
+            else
+            {
+                return "危险";
+            }
         }
     }
 }
