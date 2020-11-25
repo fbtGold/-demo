b41ce043160322115ffe7ae148fac533fd579fd3
fbt <fbtgold@foxmail.com>
2020-11-25 10:25:57 +0800

    使用了Mog

--- InterfaceExample2.Tests/UnitTest1.cs
+++ InterfaceExample2.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using 单元测试demo;
+using Moq;
 
 namespace InterfaceExample2.Tests
 {
@@ -8,7 +9,11 @@ namespace InterfaceExample2.Tests
         [Fact]
         public void PowerLowerThanZero_OK()
         {
-            var fan = new DeskFan(new PowerSupplyLowerThanZero());
+            var mock = new Mock<IPowerSupply>();
+            mock.Setup(ps => ps.GetPower()).Returns(() => 0);
+
+            //var fan = new DeskFan(new PowerSupplyLowerThanZero());
+            var fan = new DeskFan(mock.Object);
             var expected = "没电";
             var actual = fan.word();
             Assert.Equal(expected, actual);
@@ -17,26 +22,15 @@ namespace InterfaceExample2.Tests
         [Fact]
         public void PowerHigherThan220_Ok()
         {
-            var fan = new DeskFan(new PowerSupplyHigherThan220());
+            var mock = new Mock<IPowerSupply>();
+            mock.Setup(ps => ps.GetPower()).Returns(() => 220);
+
+            var fan = new DeskFan(mock.Object);
             var actual = fan.word();
             var expected = "危险";
             Assert.Equal(actual, expected);
         }
     }
 
-    class PowerSupplyLowerThanZero:IPowerSupply
-    {
-        public int GetPower()
-        {
-            return 0;
-        }
-    }
-
-    class PowerSupplyHigherThan220 : IPowerSupply
-    {
-        public int GetPower()
-        {
-            return 220;
-        }
-    }
+    
 }
